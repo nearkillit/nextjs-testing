ac825c6e5b67a8109bcba2038974878c26ab4f5e
nearkillit <tatantantatantatatantan@yahoo.co.jp>
2021-11-05 14:35:39 +0900

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.tsx','./pages/*.tsx', './components/**/*.tsx'],  
+  darkMode: false,
+  theme: {
+      extend: {},
+  },
+  variants: {
+      extend: {},
+  },
+  plugins: [],
+}
